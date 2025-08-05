387c5bb1a350ee464140f0cb51418c3d8388c746
Yudha Febri Rastuama <yudhafebrirastuama@gmail.com>
2025-08-05 11:30:44 +0700

    Test shutdown and it works

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
-<script setup></script>
+<script setup>
+
+import {invoke} from "@tauri-apps/api/core";
+
+const shutPcDown = (command) => {
+  // if (command === 'shutdown') {
+  //   willShutdown.value = true;
+  //   shutdownCt.value = 60;
+  //   shutdownTimer = setInterval(() => {
+  //     shutdownCt.value--;
+  //     if (shutdownCt.value <= 0) {
+  //       clearInterval(shutdownTimer);
+  //     }
+  //   }, 1000);
+  // }
+  // else if (command === 'cancelShutdown') {
+  //   willShutdown.value = false;
+  //   clearInterval(shutdownTimer);
+  // }
+  invoke('trigger_shutdown_state', { command }).catch(console.error);
+};
+
+const exitApp = () => {
+  invoke('closing_app', { code: 1 });
+};
+</script>
 
 <template>
   <h1 class="font-extrabold">Hello</h1>
+  <p @click="exitApp">Close</p>
+  <p @click="shutPcDown('sleep')">Sleep</p>
 </template>
