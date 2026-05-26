483016ccc619097a0a5630ab23e53d6e097b3f41
Yudha Febri Rastuama <yudhafebrirastuama@gmail.com>
2026-05-26 08:38:43 +0700

    return lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,20 +1,101 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::process::Command;
+use tauri::command;
+
+#[command]
+fn trigger_shutdown_state(command: &str) -> Result<(), String> {
+    let output = match command {
+        "sleep" => {
+            if cfg!(target_os = "windows") {
+                Command::new("rundll32.exe")
+                    .args(["powrprof.dll,SetSuspendState", "0,1,0"])
+                    .output()
+            } else if cfg!(target_os = "macos") {
+                Command::new("pmset").arg("sleepnow").output()
+            } else if cfg!(target_os = "linux") {
+                Command::new("systemctl").arg("suspend").output()
+            } else {
+                return Err("Unsupported OS".into());
+            }
+        }
+
+        "hibernate" => {
+            if cfg!(target_os = "windows") {
+                Command::new("rundll32.exe")
+                    .args(["powrprof.dll,SetSuspendState", "1,0,0"])
+                    .output()
+            } else if cfg!(target_os = "linux") {
+                Command::new("systemctl").arg("hibernate").output()
+            } else {
+                return Err("Unsupported OS".into());
+            }
+        }
+
+        "shutdown" => {
+            if cfg!(target_os = "windows") {
+                Command::new("shutdown")
+                    .args([
+                        "/s",
+                        "/f",
+                        "/t",
+                        "60",
+                        "/c",
+                        "Komputer anda akan mati dalam 60 detik :)",
+                    ])
+                    .output()
+            } else if cfg!(target_os = "macos") || cfg!(target_os = "linux") {
+                Command::new("shutdown").args(["-h", "+1"]).output()
+            } else {
+                return Err("Unsupported OS".into());
+            }
+        }
+
+        "cancelShutdown" => {
+            if cfg!(target_os = "windows") {
+                Command::new("shutdown").arg("/a").output()
+            } else if cfg!(target_os = "macos") {
+                Command::new("killall").arg("shutdown").output()
+            } else if cfg!(target_os = "linux") {
+                Command::new("shutdown").arg("-c").output()
+            } else {
+                return Err("Unsupported OS".into());
+            }
+        }
+
+        _ => return Err("Unknown command".into()),
+    };
+
+    match output {
+        Ok(out) => {
+            if out.status.success() {
+                Ok(())
+            } else {
+                Err(String::from_utf8_lossy(&out.stderr).to_string())
+            }
+        }
+        Err(err) => Err(err.to_string()),
+    }
+}
+
+#[command]
+fn closing_app(_code: u8) {
+    std::process::exit(0);
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn validate_input(user_input: String) -> bool {
+    user_input == "selm1945c3pu;"
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
-            #[cfg(desktop)]
-            app.handle()
-                .plugin(tauri_plugin_updater::Builder::new().build())?;
-            Ok(())
-        })
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .invoke_handler(tauri::generate_handler![
+            trigger_shutdown_state,
+            closing_app,
+            validate_input
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
